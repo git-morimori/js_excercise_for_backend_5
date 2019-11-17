1f62f63624652b3a420d0cb7dfe87c3f5a6b2aa4
haruka-g <mortndpgj@gmail.com>
2019-11-17 19:49:56 +0900

    最初のコミット

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = require('./router');
+const app = express();
+const PORT = 3001;
+
+app.use('/pages', router);
+app.listen(PORT, () => {
+    console.log(`Expmple app listening on port ${PORT}`);
+})
